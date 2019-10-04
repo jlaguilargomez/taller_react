0b78dcaf56bd42e8310648459dcae689b46499cf
Jose Luis Aguilar <jlaguigom@gmail.com>
2019-10-05 00:10:07 +0200

    Updates

---
+++ precache-manifest.72f6a3739b2d137463a1e7ca6258e0d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a0e1868736823718d5f06f37d89b3c3",
+    "url": "/taller_react/index.html"
+  },
+  {
+    "revision": "930de9353efd707686eb",
+    "url": "/taller_react/static/css/main.259be7c7.chunk.css"
+  },
+  {
+    "revision": "73768a28e2427ad8697c",
+    "url": "/taller_react/static/js/2.84f77bae.chunk.js"
+  },
+  {
+    "revision": "930de9353efd707686eb",
+    "url": "/taller_react/static/js/main.1f8b36d3.chunk.js"
+  },
+  {
+    "revision": "40d177e4a592253992a1",
+    "url": "/taller_react/static/js/runtime-main.7a4f2211.js"
+  }
+]);
